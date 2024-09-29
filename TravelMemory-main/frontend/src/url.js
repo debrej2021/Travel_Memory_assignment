23cb851bdcc3baeed4cca0d8cb2406eaa551d9ab
debrej2021 <98643892+debrej2021@users.noreply.github.com>
2024-09-29 23:33:20 +0530

    commited

--- TravelMemory-main/frontend/src/url.js
+++ TravelMemory-main/frontend/src/url.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
+export const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001" || "http://34.213.0.228:3001";
